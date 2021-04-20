74ae307589a99faee20c056e076cca782bc6e1b6
Makssshow <makssshowwork@gmail.com>
2021-04-20 12:15:02 +0200

    Ready 1

--- assets/js/swiper-about.js
+++ assets/js/swiper-about.js
@@ -1,23 +1,25 @@
-
-	var pop = new Swiper(".pop", {
-    loop: false,
-   // slidesPerView: "auto"
-	});
+$(document).ready(function () {
   var swiper = new Swiper(".swiper-container.two", {
     effect: "coverflow",
+    grabCursor: true,
     loop: false,
     centeredSlides: true,
     slidesPerView: "auto",
-    coverflow: {
+
+    coverflowEffect: {
       rotate: 0,
-      stretch: 100,
-      depth: 150,
-      modifier: 1.5,
-      slideShadows: false,
+      stretch: 0,
+      depth: 70,
+      modifier: 5,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
     },
   });
-
-  swiper.controller.control = pop;
-  pop.controller.control = swiper;
-
-
+  $(".swiper-slide:first-child .card-time").before("<div class='banner-alert-slide'>SLIDE TO START INTERACT</div>");
+  swiper.on("touchStart", () => {
+    $(".banner-alert-slide").fadeOut("slow");
+  });
+});
